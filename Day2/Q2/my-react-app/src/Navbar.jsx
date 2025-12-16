b823c203a4932ba37e64d67646b8eeb328a514d5
KNiveda12 <nivedavarma1296@gmail.com>
2025-12-16 22:25:35 +0530

    All changes commited

---
+++ Day2/Q2/my-react-app/src/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#333",
+        color: "white",
+        padding: "15px",
+        textAlign: "center"
+      }}
+    >
+      This is Navbar
+    </div>
+  );
+}
+
+export default Navbar;
